f43b7023d0a1abe3b83335ff24b05277472029e9
Tom Witkowski <dev.gummibeer@gmail.com>
2020-10-04 00:23:53 +0200

    add missing method to fileable interface

--- src/Contracts/Fileable.php
+++ src/Contracts/Fileable.php
@@ -3,8 +3,11 @@
 namespace Astrotomic\Fileable\Contracts;
 
 use Astrotomic\Fileable\FileAdder;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 interface Fileable
 {
+    public function files(): MorphMany;
+
     public function addFile($file): FileAdder;
 }
